5e7b5c7be51e0608e5a31591a4c0124be03b128a
benedekhalaj <bendihalaj@gmail.com>
2021-11-23 11:11:29 +0100

    Add main page question display

--- server.py
+++ server.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route("/list")
 def list_questions():
-    return "Hello World!"
+    questions = []
+    for _ in range(5):
+        questions.append({'id': '0', 'type': 'test', 'name': 'zsu'})
+    return render_template('list.html', questions=questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',
+            debug=True,
+            port=8000)
