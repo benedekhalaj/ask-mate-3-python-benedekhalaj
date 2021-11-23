a3936c966f3c8e5f80d716d354b1f2c04169c5a8
dudaskobende <dudaskobende@gmail.com>
2021-11-23 11:16:55 +0100

    Data_manager: Add file reader function

---
+++ connection.py
@@ -0,0 +1,19 @@
+import csv
+
+def open_file(file_path):
+    with open(file_path, "r") as file:
+        data_list = []
+        csv_dict = csv.DictReader(file)
+        for dictionary in csv_dict:
+            data_list.append(dictionary)
+    return data_list
+
+
+
+if __name__ == "__main__":
+    answers = 'data/answers.csv'
+    questions = 'data/questions.csv'
+    questions = open_file(questions)
+    answers = open_file(answers)
+
+
--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,8 @@
 import connection
 
+QUESTIONS_FILE_PATH = 'data/questions.csv'
+ANSWERS_FILE_PATH = 'data/answers.csv'
+DATA_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
+
 def read_csv_file():
-    pass
+    questions_list: list[dict[str]: str] = connection.open_file()
