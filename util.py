131f307631bc7f003beab550cc9103d3d412dcc8
benedekhalaj <bendihalaj@gmail.com>
2021-12-07 19:01:28 +0100

    Delete unnecessary functions in util

--- util.py
+++ util.py
@@ -13,50 +13,10 @@ def get_data_by_id(data_list, value, id):
             return data
 
 
-def get_data_and_index_by_id(data_list, value, id):
-    for index, data in enumerate(data_list):
-        if data[value] == int(id):
-            return data, index
-
-
-def get_data_list(data_list, value, id):
-    return [data for data in data_list if data[value] == int(id)]
-
-
-def modify_number(data, value, operator):
-    if operator == '+':
-        new_value = int(data[value]) + 1
-        return str(new_value)
-    else:
-        new_value = int(data[value]) - 1
-        return str(new_value)
-
-
 def update_data_by_form(data, form):
     for key, value in form.items():
         data[key] = value
     return data
-
-
-def add_new_data(data, data_list):
-    data_list.append(data)
-    return data_list
-
-
-def delete_data(data_list, data_id, value='id'):
-    for data in data_list:
-        if data_id == data[value]:
-            data_list.remove(data)
-            if data['image']:
-                delete_file('answers', data['id'])
-
-    return data_list
-
-
-def change_vote_number(data_list, data_id, operator):
-    data, index = get_data_and_index_by_id(data_list, 'id', data_id)
-    data_list[index]['vote_number'] = modify_number(data, 'vote_number', operator)
-    return data_list
 
 
 def upload_file(request_attributes, r_type='questions'):
