d3ee42ea7025b342b1e818e5617d0e9aed6afda7
benedekhalaj <bendihalaj@gmail.com>
2022-01-06 14:00:46 +0100

    Add filter bonus question tag include

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -26,16 +26,26 @@ function getSortedItems(items, sortField, sortDirection) {
 // you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
 function getFilteredItems(items, filterValue) {
     let filteredItems = [];
+
     for (let item of items) {
         if (filterValue[0] === '!') {
             if (Object.values(item).every((string) => valueNotInAnyString(string, filterValue))) {
-                filteredItems.push(item)
+                filteredItems.push(item);
             }
         }
         else {
-            if (Object.values(item).some((string) => valueInAnyString(string, filterValue))) {
-                filteredItems.push(item)
+            if (filterValue.includes(':')) {
+                let [tag, value] = filterValue.split(':');
+                if (item[tag].includes(value)) {
+                    filteredItems.push(item)
+                }
             }
+            else {
+                if (Object.values(item).some((string) => valueInAnyString(string, filterValue))) {
+                filteredItems.push(item);
+                }
+            }
+
         }
     }
     return filteredItems
